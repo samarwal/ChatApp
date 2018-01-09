4472193abe78b46909887b8198c2daf1fd2146e1
samgit22 <32052305+samgit22@users.noreply.github.com>
2018-01-08 19:10:09 -0600

    Add files via upload

---
+++ ChatMessage.java
@@ -0,0 +1,49 @@
+package com.example.samarth.chatapp;
+
+import java.util.Date;
+
+/**
+ * Created by SAMARTH on 2017-05-05.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+
+    }
+
+    public ChatMessage() {
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
